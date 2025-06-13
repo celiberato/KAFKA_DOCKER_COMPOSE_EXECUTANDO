22bddc386e09da31b7588d55297612be1aedc566
Carlos <celiberato@hotmail.com>
2025-06-13 18:24:30 -0300

    refactoring

--- src/main/java/com/interview/demo/kafka/KafkaDemoProducer.java
+++ src/main/java/com/interview/demo/kafka/KafkaDemoProducer.java
@@ -4,15 +4,14 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaDemoProducer { 
 	
 	public String executar() {
 		Properties props = new Properties(); 
 		props.put("bootstrap.servers", "localhost:9092"); 
-		props.put("key.serializer", StringSerializer.class); 
-		props.put("value.serializer", StringSerializer.class);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 	    KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 	    

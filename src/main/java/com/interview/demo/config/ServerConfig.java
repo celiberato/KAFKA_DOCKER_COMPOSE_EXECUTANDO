d6d1d95a6fede24eb44be656a1a4b9a4addf6752
Carlos <celiberato@hotmail.com>
2025-06-15 13:07:04 -0300

    refactoring

--- src/main/java/com/interview/demo/config/ServerConfig.java
+++ src/main/java/com/interview/demo/config/ServerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+@ConfigurationProperties("server")
 public class ServerConfig {
 
 	@Value("${server.port}")

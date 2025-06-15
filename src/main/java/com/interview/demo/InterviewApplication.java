9db346a204854ae879400cc7b0e0ada7d32eaeb7
Carlos <celiberato@hotmail.com>
2025-06-15 12:40:35 -0300

    refactoring

--- src/main/java/com/interview/demo/InterviewApplication.java
+++ src/main/java/com/interview/demo/InterviewApplication.java
@@ -3,8 +3,12 @@ package com.interview.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.interview.demo.config.ServerConfig;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableConfigurationProperties(ServerConfig.class)
 public class InterviewApplication {
 
 	public static void main(String[] args) {
